e43901c9bbfe5793abaf6b740cef88f1f505a440
isinvon <2021469084@qq.com>
2024-12-03 01:30:00 +0800

    style: 添加 HeaderMenu 组件的两种新样式备份
    - 新增 index_other_style_v3.vue 文件，实现字体居中版本的菜单样式- 新增 index_other_style_v2.vue 文件，实现字体靠左版本的菜单样式
    - 这两种样式为 HeaderMenu 组件提供了不同的布局和视觉效果

---
+++ home/src/views/components/HeaderMenu/index_other_style_v2.vue
@@ -0,0 +1,98 @@
+<!--字体靠左版本-->
+<!--<style lang="less" scoped>-->
+<!--#menu {-->
+<!--  background: #F6F8FA;-->
+<!--  height: 54px;-->
+<!--  padding-left: 18px;-->
+<!--  border-radius: 10px;-->
+<!--  border: 1px solid #D1D9E0;-->
+<!--}-->
+<!--#menu ul, #menu li {-->
+<!--  margin: 0 auto;-->
+<!--  padding: 0;-->
+<!--  list-style: none-->
+<!--}-->
+<!--#menu ul {-->
+<!--  width: 100%;-->
+<!--  text-align: left;-->
+<!--}-->
+<!--#menu li {-->
+<!--  display: inline-block;-->
+<!--  position: relative;-->
+<!--}-->
+<!--#menu a {-->
+<!--  display: block;-->
+<!--  line-height: 52px;-->
+<!--  padding: 0 14px;-->
+<!--  text-decoration: none;-->
+<!--  color: #59636E;-->
+<!--  font-size: 12px;-->
+<!--}-->
+<!--#menu a.dropdown-arrow:after {-->
+<!--  content: "\25BE";-->
+<!--  margin-left: 5px;-->
+<!--}-->
+<!--#menu li a:hover {-->
+<!--  color: #59636E;-->
+<!--  background: #EAEDF0;-->
+<!--}-->
+<!--#menu input {-->
+<!--  display: none;-->
+<!--  margin: 0;-->
+<!--  padding: 0;-->
+<!--  height: 54px;-->
+<!--  width: 100%;-->
+<!--  opacity: 0;-->
+<!--  cursor: pointer-->
+<!--}-->
+<!--#menu label {-->
+<!--  display: none;-->
+<!--  line-height: 52px;-->
+<!--  text-align: center;-->
+<!--  position: absolute;-->
+<!--  left: 35px-->
+<!--}-->
+<!--#menu label:before {-->
+<!--  font-size: 1.6em;-->
+<!--  color: #59636E;-->
+<!--  content: "\2261";-->
+<!--  margin-left: 20px;-->
+<!--}-->
+<!--#menu ul.sub-menus{-->
+<!--  height: auto;-->
+<!--  overflow: hidden;-->
+<!--  width: 170px;-->
+<!--  background: #FFFFFF;-->
+<!--  position: absolute;-->
+<!--  z-index: 99;-->
+<!--  display: none;-->
+<!--}-->
+<!--#menu ul.sub-menus li {-->
+<!--  display: block;-->
+<!--  text-align: left;-->
+<!--  width: 100%;-->
+<!--}-->
+<!--#menu ul.sub-menus a {-->
+<!--  color: #59636E;-->
+<!--  font-size: 12px;-->
+<!--}-->
+<!--#menu li:hover ul.sub-menus {-->
+<!--  display: block-->
+<!--}-->
+<!--#menu ul.sub-menus a:hover{-->
+<!--  background: #F2F3F4;-->
+<!--  color: #1F2328;-->
+<!--}-->
+<!--@media screen and (max-width: 800px){-->
+<!--  #menu {position:relative}-->
+<!--  #menu ul {background:#FFFFFF;position:absolute;top:100%;right:0;left:0;z-index:3;height:auto;display:none;text-align:left;}-->
+<!--  #menu ul.sub-menus {width:100%;position:static;}-->
+<!--  #menu ul.sub-menus a {padding-left:30px;}-->
+<!--  #menu li {display:block;float:none;width:auto;}-->
+<!--  #menu input, #menu label {position:absolute;top:0;left:0;display:block}-->
+<!--  #menu input {z-index:4}-->
+<!--  #menu input:checked + label {color:#59636E}-->
+<!--  #menu input:checked + label:before {content:"\00d7"}-->
+<!--  #menu input:checked ~ ul {display:block}-->
+<!--}-->
+<!--</style>-->
