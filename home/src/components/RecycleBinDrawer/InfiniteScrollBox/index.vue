31be9c734322a9e5efd8fe82e2cec9463297dafb
isinvon <2021469084@qq.com>
2024-12-05 01:40:20 +0800

    feat(RecycleBinDrawer): 添加 InfiniteScrollBox 组件
    - 新增 InfiniteScrollBox 组件，用于实现无限滚动功能
    - 组件接收父组件传递的 loadMore 方法
    - 设置组件样式，包括高度、滚动条和列表项样式

---
+++ home/src/components/RecycleBinDrawer/InfiniteScrollBox/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul
+      v-infinite-scroll="loadMore"
+      class="infinite-list"
+      style="overflow: auto"
+  >
+    <slot></slot>
+  </ul>
+</template>
+
+<script setup>
+/**
+ * 无限滚动列表组件
+ * @author sinvon
+ * @since 2024年12月5日01:39:34
+ * @module InfiniteScrollBox
+ */
+
+// 接收父组件传递的 `loadMore` 方法
+defineProps({
+  loadMore: {
+    type: Function,
+    required: true,
+  },
+})
+</script>
+
+<style scoped lang="less">
+.infinite-list {
+  height: 300px;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  overflow: auto;
+
+}
+
+.infinite-list-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+</style>
