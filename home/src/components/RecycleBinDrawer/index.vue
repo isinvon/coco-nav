975e2cea48acca6cd8285f50f62837dd95372dc8
isinvon <2021469084@qq.com>
2024-12-05 01:41:40 +0800

    feat(RecycleBinDrawer): 实现回收站书签列表和清空功能
    - 添加书签列表组件，支持无限滚动加载
    - 实现清空回收站功能，增加用户确认步骤
    - 添加单个书签永久删除功能
    - 优化组件样式，增加全局样式变量

--- home/src/components/RecycleBinDrawer/index.vue
+++ home/src/components/RecycleBinDrawer/index.vue
@@ -5,16 +5,98 @@
  * @author sinvon
  * @since 2024年12月4日22:12:10
  */
-import {useRecycleBinDrawerStore} from '@/store/recycleBinDrawer'
+import InfiniteScroll from './InfiniteScrollBox/index.vue';
+import {useRecycleBinDrawerStore} from '@/store/recycleBinDrawer';
+import {ref} from 'vue';
+import {throttle} from 'lodash';
+import {ElMessageBox, ElMessage} from 'element-plus';
+import {Delete} from "@element-plus/icons-vue";
 
-const recycleBinDrawerStore = useRecycleBinDrawerStore()
+const recycleBinDrawerStore = useRecycleBinDrawerStore();
+const bookmarks = ref([
+  // 模拟初始数据
+  {
+    title: '菜鸟教程1111111111111111111111111',
+    url: 'https://www.runoob.com/',
+    icon: 'https://www.runoob.com/favicon.ico',
+    description: '菜鸟教程 - 免费的在线编程教程111111111111111111111111111'
+  },
+  {
+    title: '开源中国',
+    url: 'https://www.oschina.net/',
+    icon: 'https://www.oschina.net/favicon.ico',
+    description: 'Vue.js - The Progressive JavaScript Framework'
+  }
+]);
+
+// 节流加载更多数据
+const throttledLoad = throttle(() => {
+  const mockData = Array.from({length: 5}, (_, i) => ({
+    title: `书签 ${bookmarks.value.length + i + 1}`,
+    url: `https://example${bookmarks.value.length + i + 1}.com`,
+    icon: 'https://via.placeholder.com/16',
+    description: `这是书签 ${bookmarks.value.length + i + 1}`
+  }));
+  bookmarks.value.push(...mockData);
+}, 300);
+
+// 清空回收站功能
+const clearRecycleBin = () => {
+  ElMessageBox.prompt(
+      '请输入您的名字以确认清空回收站',
+      '确认清空',
+      {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        inputPlaceholder: '输入名字'
+      }
+  )
+      .then(({value}) => {
+        if (value) {
+          bookmarks.value = [];
+          ElMessage.success('回收站已清空');
+        } else {
+          ElMessage.error('请输入名字以确认清空');
+        }
+      })
+      .catch(() => ElMessage.info('清空操作已取消'));
+};
+
+// 永久删除单个书签
+const deleteBookmark = (index) => {
+  bookmarks.value.splice(index, 1);
+  ElMessage.success('书签已永久删除');
+};
 </script>
 
 <template>
-  <!--<el-button type="primary" style="margin-left: 16px" @click="drawer = true">-->
-  <!--  open-->
-  <!--</el-button>-->
-  <el-drawer direction="rtl" v-model="recycleBinDrawerStore.isOpen" title="我是右边的抽屉" :with-header="false">
-    <span>Hi there!</span>
+  <el-drawer class="recycle-bin-drawer" direction="rtl" v-model="recycleBinDrawerStore.isOpen" title="回收站"
+             :with-header="false">
+    <!-- 清空回收站按钮 -->
+    <div class="recycle-bin-header">
+      <el-button type="danger" size="large" @click="clearRecycleBin">清空回收站</el-button>
+    </div>
+
+    <!-- 无限滚动书签列表 -->
+    <InfiniteScroll class="infinite-list" :loadMore="throttledLoad">
+      <div
+          v-for="(bookmark, index) in bookmarks"
+          :key="index"
+          class="infinite-list-item"
+      >
+        <div class="bookmark-info">
+          <img :src="bookmark.icon" alt="icon" class="bookmark-icon"/>
+          <div class="bookmark-details">
+            <a :href="bookmark.url" target="_blank" class="bookmark-title">{{ bookmark.title }}</a>
+            <p class="bookmark-description">{{ bookmark.description }}</p>
+          </div>
+        </div>
+        <el-button type="danger" size="default" @click="deleteBookmark(index)">永久删除</el-button>
+      </div>
+    </InfiniteScroll>
   </el-drawer>
 </template>
+
+<style scoped lang="less">
+@import "./index";
+</style>
