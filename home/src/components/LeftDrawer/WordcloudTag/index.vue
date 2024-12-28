4614cdf489d375ebe8860e4f3dc838923c7f529f
isinvon <2021469084@qq.com>
2024-12-29 00:10:16 +0800

    feat(components): 新增词云组件
    - 添加 WordcloudTag 组件用于展示词云效果
    -组件使用 ElTag 展示标签，支持自定义样式和效果- 移除 vue-wordcloud依赖，使用自定义组件替代

---
+++ home/src/components/LeftDrawer/WordcloudTag/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wordcloud-container">
+    <el-tag
+        v-for="(bookmark, index) in data"
+        :key="index"
+        :type="getRandomTagType()"
+        :effect="effect"
+    >
+      {{ bookmark.name }}
+    </el-tag>
+  </div>
+</template>
+
+<script lang="js" setup>
+
+/**
+ * @author sinvon
+ * @description 词云组件
+ * @since 2024年12月29日00:04:41
+ * @type {Prettify<Readonly<ExtractPropTypes<{data: {type: ArrayConstructor, required: boolean}}>>>}
+ */
+const props = defineProps({
+  /**
+   * 数据
+   */
+  data: {
+    type: Array,
+    required: true
+  },
+  /**
+   * tag样式
+   * @type {'dark'|'plain'|'light'}
+   *
+   */
+  effect: {
+    type: String,
+    default: 'light'
+  }
+});
+
+const getRandomTagType = () => {
+  const types = ['success', 'info', 'warning', 'danger'];
+  return types[Math.floor(Math.random() * types.length)];
+};
+</script>
+
+<style lang="less" scoped>
+.wordcloud-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  padding: 10px;
+}
+</style>
+
+
+
