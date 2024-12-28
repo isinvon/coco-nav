0924c99c842a9d81d27fea8da48571079b1a9b4c
isinvon <2021469084@qq.com>
2024-12-29 01:35:01 +0800

    feat: 添加头部菜单枚举
    - 新增 headerMenuEnum 数组，包含多个菜单项对象
    - 每个菜单项包含 name、icon、title 和 path 属性
    - 菜单项包括首页、关于、归档、标签、分类、友链、留言和搜索

---
+++ home/src/enum/headerMenuEunm.js
@@ -0,0 +1,50 @@
+export const headerMenuEnum = [
+    {
+        name: 'home',
+        icon: '🏠',
+        title: '首页',
+        path: '/'
+    },
+    {
+        name: 'about',
+        icon: '🔍',
+        title: '关于',
+        path: '/about'
+    },
+    {
+        name: 'archives',
+        icon: '📚',
+        title: '归档',
+        path: '/archives'
+    },
+    {
+        name: 'tag',
+        icon: '🏷️',
+        title: '标签',
+        path: '/tag'
+    },
+    {
+        name: 'category',
+        icon: '📂',
+        title: '分类',
+        path: '/category'
+    },
+    {
+        name: 'friendlink',
+        icon: '🤝',
+        title: '友链',
+        path: '/friendlink'
+    },
+    {
+        name: 'message',
+        icon: '💬',
+        title: '留言',
+        path: '/message'
+    },
+    {
+        name: 'search',
+        icon: '🔍',
+        title: '搜索',
+        path: '/search'
+    },
+]
