8d2be93368fd82d46c9457a1ac04bc928a0bd723
isinvon <2021469084@qq.com>
2024-12-29 00:57:18 +0800

    feat(LeftDrawer): 添加徽章,并根据浏览量动态分配标签类型
    - 添加浏览量徽章显示
    - 调整标签样式，增加徽章位置和样式

--- home/src/components/LeftDrawer/components/WordcloudTag.vue
+++ home/src/components/LeftDrawer/components/WordcloudTag.vue
@@ -3,22 +3,21 @@
     <el-tag
         v-for="(bookmark, index) in data"
         :key="index"
-        :type="getRandomTagType()"
+        :type="getTypeByView(bookmark.view)"
         :effect="effect"
+        class="tag-with-badge"
     >
       {{ bookmark.name }}
+      <el-badge :value="bookmark.view" :max="999" class="badge-inside-tag">
+        <i></i> <!-- Placeholder for badge -->
+      </el-badge>
     </el-tag>
   </div>
 </template>
 
 <script lang="js" setup>
+import {defineProps, computed} from 'vue';
 
-/**
- * @author sinvon
- * @description 词云组件
- * @since 2024年12月29日00:04:41
- * @type {Prettify<Readonly<ExtractPropTypes<{data: {type: ArrayConstructor, required: boolean}}>>>}
- */
 const props = defineProps({
   /**
    * 数据
@@ -38,18 +37,51 @@ const props = defineProps({
   }
 });
 
-const getRandomTagType = () => {
-  const types = ['success', 'info', 'warning', 'danger'];
-  return types[Math.floor(Math.random() * types.length)];
+// 计算最小值和最大值
+const minView = computed(() => Math.min(...props.data.map(item => item.view)));
+const maxView = computed(() => Math.max(...props.data.map(item => item.view)));
+
+const getTypeByView = (view) => {
+  if (view === minView.value) {
+    return 'success'; // 最小值对应的类型
+  } else if (view === maxView.value) {
+    return 'danger'; // 最大值对应的类型
+  } else {
+    const range = maxView.value - minView.value;
+    const normalizedValue = (view - minView.value) / range;
+    if (normalizedValue <= 0.33) {
+      return 'info';
+    } else if (normalizedValue <= 0.66) {
+      return 'warning';
+    } else {
+      return 'primary';
+    }
+  }
 };
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
 .wordcloud-container {
   display: flex;
   flex-wrap: wrap;
   gap: 10px;
   padding: 10px;
+}
+
+.tag-with-badge {
+  position: relative;
+  margin: 5px;
+}
+
+// 徽章的位置
+.badge-inside-tag {
+  position: absolute;
+  top: -1px;
+  right: 0;
+  // 徽章样式
+  :deep(.el-badge__content) {
+    //background: #c44390;
+  }
 }
 </style>
 
