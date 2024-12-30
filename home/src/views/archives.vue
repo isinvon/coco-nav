287555a1788dae2c19681fa65a79c9d2a55ee755
isinvon <2021469084@qq.com>
2024-12-31 00:00:59 +0800

    feat(archives): 添加归档页面组件
    - 创建 Archives 组件，支持两种归档显示样式
    - 使用 templateVersion 控制显示不同的归档版本
    - 在组件挂载时从 SettingData 中获取归档模板设置

---
+++ home/src/views/archives.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Archives_v1 v-if="templateVersion === 1"/>
+    <Archives_v2 v-if="templateVersion === 2"/>
+  </div>
+</template>
+
+<script setup>
+import Archives_v1 from "@/views/arhivesComponents/archives_v1.vue";
+import Archives_v2 from "@/views/arhivesComponents/archives_v2.vue";
+import * as SettingData from '../../mock/setting.js';
+import {onMounted, ref} from "vue";
+
+// 归档的显示样式
+const templateVersion = ref(0);
+
+onMounted(() => {
+  templateVersion.value = SettingData.setting.data.archives.template;
+});
+</script>
+
+<style lang="less" scoped>
+
+</style>
