c06327fa913e2272955f15eabe3338b3a43b9f61
isinvon <2021469084@qq.com>
2024-12-30 23:17:04 +0800

    feat(router): 添加友链页面路由
    - 在路由配置中添加了友链页面路由
    - 新增友链页面组件的按需加载
    - 优化了代码格式，调整了缩进和空格

--- home/src/router/index.js
+++ home/src/router/index.js
@@ -1,32 +1,39 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import home from "@/views/home.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    components: {
-      default: () => import('@/views/about.vue'),
+    {
+        path: '/',
+        name: 'home',
+        component: home,
+    },
+    {
+        path: '/about',
+        name: 'about',
+        components: {
+            default: () => import('@/views/about.vue'),
+        }
+    },
+    {
+        path: '/friendlink',
+        name: 'friendlink',
+        components: {
+            default: () => import('@/views/friendlink.vue'),
+        }
+    },
+    {
+        path: '/message',
+        name: 'message',
+        components: {
+            default: () => import('@/views/message.vue'),
+        }
     }
-  },
-  {
-    path: '/message',
-    name: 'message',
-    components: {
-      default: () => import('@/views/message.vue'),
-    }
-  }
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes, // 路由配置
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes, // 路由配置
 });
 
 export default router;
