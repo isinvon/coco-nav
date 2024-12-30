23c48d1c3c9db7d161af32c41766e9a3d484887e
isinvon <2021469084@qq.com>
2024-12-30 22:52:47 +0800

    refactor: 路由重构页面布局并优化组件结构
    - 在 App.vue 中添加全局布局结构，包括 HeaderMenu、Footer 和 HotCard组件

--- home/src/App.vue
+++ home/src/App.vue
@@ -1,12 +1,33 @@
 <script setup>
+import HeaderMenu from "@/views/components/HeaderMenu/index.vue";
+import Footer from "@/views/components/Footer/index.vue";
+import newsList from "../mock/newList.js";
+import HotCard from "@/components/HotCard/index.vue";
 </script>
 
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <!--头部菜单-->
+        <HeaderMenu/>
+      </el-header>
+      <el-main class="main-container">
+        <!--路由-->
+        <router-view></router-view>
+        <!--页脚-->
+        <Footer/>
+      </el-main>
+    </el-container>
+
+    <!--热点卡片-->
+    <transition name="hot-card-animation">
+      <HotCard class="hot-card" :newsList="newsList"/>
+    </transition>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="less" scoped>
+// 引入hotCard的样式
+@import "views/style/hotCard";
 </style>
--- home/src/views/home.vue
+++ home/src/views/home.vue
@@ -1,7 +1,5 @@
 <script setup lang="js">
-import HeaderMenu from '@/views/components/HeaderMenu/index.vue'
 import Main from '@/views/components/Main/index.vue'
-import Footer from '@/views/components/Footer/index.vue'
 import LeftDrawer from '@/components/LeftDrawer/index.vue'
 import RightDrawer from '@/components/RightDrawer/index.vue'
 import BookmarkDetailsDrawer from '@/components/BookmarkDetailsDrawer/index.vue'
@@ -9,50 +7,30 @@ import Anchor from '@/components/Anchor/index.vue'
 import AddBookmarkBox from '@/components/AddBookmarkBox/index.vue'
 import DeleteMenu from '@/components/DeleteMenu/index.vue'
 import RecycleBinDrawer from '@/components/RecycleBinDrawer/index.vue'
-import HotCard from '@/components/HotCard/index.vue'
-
-import newsList from "../../mock/newList.js";
 </script>
 <template>
   <div class="common-layout">
-    <el-container>
-      <el-header>
-        <HeaderMenu/>
-      </el-header>
-      <el-main class="main-container">
-        <!--锚点-->
-        <Anchor class="home-anchor"/>
-        <Main/>
-        <!--页脚-->
-        <Footer/>
-      </el-main>
-      <!--抽屉-->
-      <LeftDrawer/>
-      <!--抽屉-->
-      <RightDrawer/>
-      <!--标签详情抽屉-->
-      <BookmarkDetailsDrawer/>
-      <!--添加书签的框-->
-      <AddBookmarkBox/>
-      <!--删除书签环绕菜单 (内部逻辑: 状态为正在删除的时候触发)-->
-      <DeleteMenu class="delete-menu"/>
-      <!--回收站抽屉组件-->
-      <RecycleBinDrawer/>
-      <!--热点卡片-->
-      <transition name="hot-card-animation">
-        <HotCard class="hot-card" :newsList="newsList"/>
-      </transition>
-    </el-container>
+    <!--锚点-->
+    <Anchor class="home-anchor"/>
+    <Main/>
+    <!--抽屉-->
+    <LeftDrawer/>
+    <!--抽屉-->
+    <RightDrawer/>
+    <!--标签详情抽屉-->
+    <BookmarkDetailsDrawer/>
+    <!--添加书签的框-->
+    <AddBookmarkBox/>
+    <!--删除书签环绕菜单 (内部逻辑: 状态为正在删除的时候触发)-->
+    <DeleteMenu class="delete-menu"/>
+    <!--回收站抽屉组件-->
+    <RecycleBinDrawer/>
+
   </div>
 </template>
 
 <style scoped lang="less">
-// 引入hotCard的样式
-@import "style/hotCard";
 
-.main-container {
-  position: relative;
-}
 
 .home-anchor {
   position: fixed;
