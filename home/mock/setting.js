e469620e3d9193b761411f2416a2d9fa8883a168
isinvon <2021469084@qq.com>
2024-12-30 16:59:09 +0800

    mock(setting): 添加网站设置模拟数据
    - 新增 setting.js 文件，用于模拟网站设置数据
    - 设置数据包括留言区和评论区的相关配置
    - 为后续功能开发和测试提供模拟数据支持

---
+++ home/mock/setting.js
@@ -0,0 +1,17 @@
+// 网站设置模拟数据
+const setting = {
+    status: "成功",
+    code: 200,
+    data: {
+        // 留言区
+        message: {
+            // 评论区
+            comment: {
+                // 是否显示等级
+                show_level: true,
+            }
+        }
+    }
+};
+
+export {setting}
