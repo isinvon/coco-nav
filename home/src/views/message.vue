9adb8f699a90a79b9da60c0722af4a00ad11c24e
isinvon <2021469084@qq.com>
2024-12-31 17:44:05 +0800

    refactor(message): 移除消息页面冗余代码
    - 删除了无用的 grid-content 类 div元素
    - 移除了未使用的 el-row 和 el-col 样式

--- home/src/views/message.vue
+++ home/src/views/message.vue
@@ -3,10 +3,8 @@
     <!--左边-->
     <el-row :gutter="20">
       <el-col :span="3">
-        <div class="grid-content ep-bg-purple"/>
       </el-col>
       <el-col :span="12">
-        <div class="grid-content ep-bg-purple"/>
         <!-- 评论区整体结构 -->
         <div class="comment-section">
           <!-- 评论显示区 -->
@@ -20,7 +18,6 @@
       </el-col>
       <!--右边-->
       <el-col :span="5">
-        <div class="grid-content ep-bg-purple"/>
         <RightAside/>
       </el-col>
     </el-row>
@@ -118,21 +115,4 @@ const submitComment = () => {
 }
 
 // ------------------
-
-.el-row {
-  margin-bottom: 20px;
-}
-
-.el-row:last-child {
-  margin-bottom: 0;
-}
-
-.el-col {
-  border-radius: 4px;
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
 </style>
