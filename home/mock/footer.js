3ad2e7dec1718ee91744d0096177f0460c8f9add
isinvon <2021469084@qq.com>
2024-12-31 22:53:07 +0800

    mock: 添加 footer 组件的模拟数据

---
+++ home/mock/footer.js
@@ -0,0 +1,23 @@
+// footer数据
+const footer = {
+    status: "成功",
+    code: 200,
+    data: {
+        links: [
+            {text: "Home", url: "#"},
+            {text: "About", url: "#"},
+            {text: "Terms", url: "#"},
+            {text: "Privacy", url: "#"},
+            {text: "Security", url: "#"},
+            {text: "Status", url: "#"},
+            {text: "Docs", url: "#"},
+            {text: "Contact GitHub", url: "#"},
+            {text: "Pricing", url: "#"},
+            {text: "API", url: "#"},
+            {text: "Training", url: "#"},
+            {text: "Blog", url: "#"}
+        ]
+    }
+};
+
+export {footer}
