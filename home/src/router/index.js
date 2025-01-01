af4d56d4a9ca86d030df1df8f96e75368b872308
isinvon <2021469084@qq.com>
2025-01-01 16:22:47 +0800

    feat(router): 添加页面切换进度条
    - 在 router/index.js 中引入 NProgress 并配置
    - 添加全局前置守卫和后置钩子以控制进度条显示
    - 在 styles 目录下新增 nprogress-bar.less 以自定义进度条样式
    - 在 package.json 中添加 nprogress 依赖

--- home/src/router/index.js
+++ home/src/router/index.js
@@ -1,6 +1,19 @@
 // src/router/index.js
 import {createRouter, createWebHistory} from 'vue-router';
 import home from "@/views/home.vue";
+// 进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'// nprogress样式文件
+import '@/styles/nprogress-bar.less' // 自定义的nprogress样式文件
+// 进度条配置
+NProgress.configure({
+    easing: 'ease',  // 动画方式
+    speed: 500,  // 递增进度条的速度
+    showSpinner: false, // 是否显示加载ico
+    trickleSpeed: 200, // 自动递增间隔
+    minimum: 0.3 // 初始化时的最小百分比
+})
+
 
 const routes = [
     {
@@ -56,5 +69,19 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes, // 路由配置
 });
+
+
+//当路由开始跳转时
+router.beforeEach((to, from, next) => {
+    // 开启进度条
+    NProgress.start();
+    // 这个一定要加，没有next()页面不会跳转的。这部分还不清楚的去翻一下官网就明白了
+    next();
+});
+//当路由跳转结束后
+router.afterEach(() => {
+    // 关闭进度条
+    NProgress.done()
+})
 
 export default router;
