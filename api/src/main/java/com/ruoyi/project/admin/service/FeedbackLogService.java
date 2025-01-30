455392d67c9d96c94100c50198a3f52d2f457b17
isinvon <2021469084@qq.com>
2025-01-31 03:40:59 +0800

    refactor: 生成基础服务接口

--- api/src/main/java/com/ruoyi/project/admin/service/FeedbackLogService.java
+++ api/src/main/java/com/ruoyi/project/admin/service/FeedbackLogService.java
@@ -1,11 +1,11 @@
 package com.ruoyi.project.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.ruoyi.project.admin.domain.AdvertisementLog;
+import com.ruoyi.project.admin.domain.FeedbackLog;
 
 /**
  * @author : sinvon
  * @since :  2025/1/31 上午3:37
  */
-public interface AdvertisementLogService extends IService<AdvertisementLog> {
+public interface FeedbackLogService extends IService<FeedbackLog> {
 }
