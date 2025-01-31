8b9b6d0fbc694fa5d9fe3ec799ab7e61f0b0a8cd
isinvon <2021469084@qq.com>
2025-02-01 02:53:23 +0800

    refactor(admin): 重构书签文章控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/BookmarkArticleController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/BookmarkArticleController.java
@@ -1,5 +1,6 @@
 package com.ruoyi.project.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
@@ -9,35 +10,34 @@ import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
 import com.ruoyi.framework.web.page.TableDataInfo;
 import com.ruoyi.project.admin.domain.BookmarkArticle;
-import com.ruoyi.project.admin.service.custom.BookmarkArticleCustomService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ruoyi.project.admin.service.BookmarkArticleService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 书签文章Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/bookmarkArticle")
-public class BookmarkArticleController extends BaseController
-{
-    @Autowired
-    private BookmarkArticleCustomService bookmarkArticleCustomService;
+public class BookmarkArticleController extends BaseController {
+    @Resource
+    private BookmarkArticleService bookmarkArticleService;
 
     /**
      * 查询书签文章列表
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(BookmarkArticle bookmarkArticle)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<BookmarkArticle> list = bookmarkArticleCustomService.selectBookmarkArticleList(bookmarkArticle);
+        List<BookmarkArticle> list = bookmarkArticleService.list();
         return getDataTable(list);
     }
 
@@ -47,10 +47,9 @@ public class BookmarkArticleController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_EXPORT)
     @Log(title = "书签文章", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, BookmarkArticle bookmarkArticle)
-    {
-        List<BookmarkArticle> list = bookmarkArticleCustomService.selectBookmarkArticleList(bookmarkArticle);
-        ExcelUtil<BookmarkArticle> util = new ExcelUtil<BookmarkArticle>(BookmarkArticle.class);
+    public void export(HttpServletResponse response) {
+        List<BookmarkArticle> list = bookmarkArticleService.list();
+        ExcelUtil<BookmarkArticle> util = new ExcelUtil<>(BookmarkArticle.class);
         util.exportExcel(response, list, "书签文章数据");
     }
 
@@ -59,9 +58,8 @@ public class BookmarkArticleController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_QUERY)
     @GetMapping(value = "/{articleId}")
-    public AjaxResult getInfo(@PathVariable("articleId") Long articleId)
-    {
-        return success(bookmarkArticleCustomService.selectBookmarkArticleByArticleId(articleId));
+    public AjaxResult getInfo(@PathVariable("articleId") Long articleId) {
+        return success(bookmarkArticleService.getById(articleId));
     }
 
     /**
@@ -70,9 +68,8 @@ public class BookmarkArticleController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_ADD)
     @Log(title = "书签文章", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody BookmarkArticle bookmarkArticle)
-    {
-        return toAjax(bookmarkArticleCustomService.insertBookmarkArticle(bookmarkArticle));
+    public AjaxResult add(@RequestBody BookmarkArticle bookmarkArticle) {
+        return toAjax(bookmarkArticleService.save(bookmarkArticle));
     }
 
     /**
@@ -81,9 +78,10 @@ public class BookmarkArticleController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_EDIT)
     @Log(title = "书签文章", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody BookmarkArticle bookmarkArticle)
-    {
-        return toAjax(bookmarkArticleCustomService.updateBookmarkArticle(bookmarkArticle));
+    public AjaxResult edit(@RequestBody BookmarkArticle bookmarkArticle) {
+        LambdaQueryWrapper<BookmarkArticle> qw = new LambdaQueryWrapper<>();
+        qw.eq(BookmarkArticle::getArticleId, bookmarkArticle.getArticleId());
+        return toAjax(bookmarkArticleService.update(bookmarkArticle, qw));
     }
 
     /**
@@ -91,9 +89,9 @@ public class BookmarkArticleController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_ARTICLE_REMOVE)
     @Log(title = "书签文章", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{articleIds}")
-    public AjaxResult remove(@PathVariable Long[] articleIds)
-    {
-        return toAjax(bookmarkArticleCustomService.deleteBookmarkArticleByArticleIds(articleIds));
+    @DeleteMapping("/{articleIds}")
+    public AjaxResult remove(@PathVariable Long[] articleIds) {
+        List<Long> idList = Arrays.asList(articleIds);
+        return toAjax(bookmarkArticleService.removeByIds(idList));
     }
 }
