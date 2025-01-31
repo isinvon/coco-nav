34f2cef1256ead66d651afbb6d44e8749ebff206
isinvon <2021469084@qq.com>
2025-02-01 03:45:21 +0800

    refactor(admin): 重构留言日志控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/MessageLogController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/MessageLogController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.MessageLog;
-import com.ruoyi.project.admin.service.custom.MessageLogCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.MessageLog;
+import com.ruoyi.project.admin.service.MessageLogService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 留言操作日志Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/messageLog")
-public class MessageLogController extends BaseController
-{
-    @Autowired
-    private MessageLogCustomService messageLogCustomService;
+public class MessageLogController extends BaseController {
+    @Resource
+    private MessageLogService messageLogService;
 
     /**
      * 查询留言操作日志列表
      */
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(MessageLog messageLog)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<MessageLog> list = messageLogCustomService.selectMessageLogList(messageLog);
+        List<MessageLog> list = messageLogService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class MessageLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_EXPORT)
     @Log(title = "留言操作日志", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, MessageLog messageLog)
-    {
-        List<MessageLog> list = messageLogCustomService.selectMessageLogList(messageLog);
-        ExcelUtil<MessageLog> util = new ExcelUtil<MessageLog>(MessageLog.class);
+    public void export(HttpServletResponse response) {
+        List<MessageLog> list = messageLogService.list();
+        ExcelUtil<MessageLog> util = new ExcelUtil<>(MessageLog.class);
         util.exportExcel(response, list, "留言操作日志数据");
     }
 
@@ -66,9 +58,8 @@ public class MessageLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_QUERY)
     @GetMapping(value = "/{messageLogId}")
-    public AjaxResult getInfo(@PathVariable("messageLogId") Long messageLogId)
-    {
-        return success(messageLogCustomService.selectMessageLogByMessageLogId(messageLogId));
+    public AjaxResult getInfo(@PathVariable("messageLogId") Long messageLogId) {
+        return success(messageLogService.getById(messageLogId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class MessageLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_ADD)
     @Log(title = "留言操作日志", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody MessageLog messageLog)
-    {
-        return toAjax(messageLogCustomService.insertMessageLog(messageLog));
+    public AjaxResult add(@RequestBody MessageLog messageLog) {
+        return toAjax(messageLogService.save(messageLog));
     }
 
     /**
@@ -88,9 +78,10 @@ public class MessageLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_EDIT)
     @Log(title = "留言操作日志", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody MessageLog messageLog)
-    {
-        return toAjax(messageLogCustomService.updateMessageLog(messageLog));
+    public AjaxResult edit(@RequestBody MessageLog messageLog) {
+        LambdaQueryWrapper<MessageLog> qw = new LambdaQueryWrapper<>();
+        qw.eq(MessageLog::getMessageLogId, messageLog.getMessageLogId());
+        return toAjax(messageLogService.update(messageLog, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class MessageLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_MESSAGE_LOG_REMOVE)
     @Log(title = "留言操作日志", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{messageLogIds}")
-    public AjaxResult remove(@PathVariable Long[] messageLogIds)
-    {
-        return toAjax(messageLogCustomService.deleteMessageLogByMessageLogIds(messageLogIds));
+    @DeleteMapping("/{messageLogIds}")
+    public AjaxResult remove(@PathVariable Long[] messageLogIds) {
+        List<Long> idList = Arrays.asList(messageLogIds);
+        return toAjax(messageLogService.removeByIds(idList));
     }
 }
