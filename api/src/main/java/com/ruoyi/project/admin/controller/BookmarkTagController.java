c3200cf97c7e64939acf3dbd0535b68aa3824734
isinvon <2021469084@qq.com>
2025-02-01 03:13:06 +0800

    refactor(admin): 重构书签标签模块

--- api/src/main/java/com/ruoyi/project/admin/controller/BookmarkTagController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/BookmarkTagController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.BookmarkTag;
-import com.ruoyi.project.admin.service.custom.BookmarkTagCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.BookmarkTag;
+import com.ruoyi.project.admin.service.BookmarkTagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 书签标签Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/bookmarkTag")
-public class BookmarkTagController extends BaseController
-{
+public class BookmarkTagController extends BaseController {
     @Autowired
-    private BookmarkTagCustomService bookmarkTagCustomService;
+    private BookmarkTagService bookmarkTagService;
 
     /**
      * 查询书签标签列表
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(BookmarkTag bookmarkTag)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<BookmarkTag> list = bookmarkTagCustomService.selectBookmarkTagList(bookmarkTag);
+        List<BookmarkTag> list = bookmarkTagService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class BookmarkTagController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_EXPORT)
     @Log(title = "书签标签", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, BookmarkTag bookmarkTag)
-    {
-        List<BookmarkTag> list = bookmarkTagCustomService.selectBookmarkTagList(bookmarkTag);
-        ExcelUtil<BookmarkTag> util = new ExcelUtil<BookmarkTag>(BookmarkTag.class);
+    public void export(HttpServletResponse response) {
+        List<BookmarkTag> list = bookmarkTagService.list();
+        ExcelUtil<BookmarkTag> util = new ExcelUtil<>(BookmarkTag.class);
         util.exportExcel(response, list, "书签标签数据");
     }
 
@@ -66,9 +58,8 @@ public class BookmarkTagController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_QUERY)
     @GetMapping(value = "/{bookmarkTagId}")
-    public AjaxResult getInfo(@PathVariable("bookmarkTagId") Long bookmarkTagId)
-    {
-        return success(bookmarkTagCustomService.selectBookmarkTagByBookmarkTagId(bookmarkTagId));
+    public AjaxResult getInfo(@PathVariable("bookmarkTagId") Long bookmarkTagId) {
+        return success(bookmarkTagService.getById(bookmarkTagId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class BookmarkTagController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_ADD)
     @Log(title = "书签标签", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody BookmarkTag bookmarkTag)
-    {
-        return toAjax(bookmarkTagCustomService.insertBookmarkTag(bookmarkTag));
+    public AjaxResult add(@RequestBody BookmarkTag bookmarkTag) {
+        return toAjax(bookmarkTagService.save(bookmarkTag));
     }
 
     /**
@@ -88,9 +78,10 @@ public class BookmarkTagController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_EDIT)
     @Log(title = "书签标签", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody BookmarkTag bookmarkTag)
-    {
-        return toAjax(bookmarkTagCustomService.updateBookmarkTag(bookmarkTag));
+    public AjaxResult edit(@RequestBody BookmarkTag bookmarkTag) {
+        LambdaQueryWrapper<BookmarkTag> qw = new LambdaQueryWrapper<>();
+        qw.eq(BookmarkTag::getBookmarkTagId, bookmarkTag.getBookmarkTagId());
+        return toAjax(bookmarkTagService.update(bookmarkTag, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class BookmarkTagController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BOOKMARK_TAG_REMOVE)
     @Log(title = "书签标签", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{bookmarkTagIds}")
-    public AjaxResult remove(@PathVariable Long[] bookmarkTagIds)
-    {
-        return toAjax(bookmarkTagCustomService.deleteBookmarkTagByBookmarkTagIds(bookmarkTagIds));
+    @DeleteMapping("/{bookmarkTagIds}")
+    public AjaxResult remove(@PathVariable Long[] bookmarkTagIds) {
+        List<Long> idList = Arrays.asList(bookmarkTagIds);
+        return toAjax(bookmarkTagService.removeByIds(idList));
     }
 }
