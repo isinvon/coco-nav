da4c1a622a00385c8bbfa88fe87a827dfe65597a
isinvon <2021469084@qq.com>
2025-02-01 03:17:21 +0800

    refactor(admin): 重构用户浏览历史控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/BrowseHistoryController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/BrowseHistoryController.java
@@ -1,5 +1,6 @@
 package com.ruoyi.project.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
@@ -9,35 +10,34 @@ import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
 import com.ruoyi.framework.web.page.TableDataInfo;
 import com.ruoyi.project.admin.domain.BrowseHistory;
-import com.ruoyi.project.admin.service.custom.BrowseHistoryCustomService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ruoyi.project.admin.service.BrowseHistoryService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 用户浏览历史Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/browseHistory")
-public class BrowseHistoryController extends BaseController
-{
-    @Autowired
-    private BrowseHistoryCustomService browseHistoryCustomService;
+public class BrowseHistoryController extends BaseController {
+    @Resource
+    private BrowseHistoryService browseHistoryService;
 
     /**
      * 查询用户浏览历史列表
      */
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(BrowseHistory browseHistory)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<BrowseHistory> list = browseHistoryCustomService.selectBrowseHistoryList(browseHistory);
+        List<BrowseHistory> list = browseHistoryService.list();
         return getDataTable(list);
     }
 
@@ -47,10 +47,9 @@ public class BrowseHistoryController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_EXPORT)
     @Log(title = "用户浏览历史", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, BrowseHistory browseHistory)
-    {
-        List<BrowseHistory> list = browseHistoryCustomService.selectBrowseHistoryList(browseHistory);
-        ExcelUtil<BrowseHistory> util = new ExcelUtil<BrowseHistory>(BrowseHistory.class);
+    public void export(HttpServletResponse response) {
+        List<BrowseHistory> list = browseHistoryService.list();
+        ExcelUtil<BrowseHistory> util = new ExcelUtil<>(BrowseHistory.class);
         util.exportExcel(response, list, "用户浏览历史数据");
     }
 
@@ -59,9 +58,8 @@ public class BrowseHistoryController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_QUERY)
     @GetMapping(value = "/{browseHistoryId}")
-    public AjaxResult getInfo(@PathVariable("browseHistoryId") Long browseHistoryId)
-    {
-        return success(browseHistoryCustomService.selectBrowseHistoryByBrowseHistoryId(browseHistoryId));
+    public AjaxResult getInfo(@PathVariable("browseHistoryId") Long browseHistoryId) {
+        return success(browseHistoryService.getById(browseHistoryId));
     }
 
     /**
@@ -70,9 +68,8 @@ public class BrowseHistoryController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_ADD)
     @Log(title = "用户浏览历史", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody BrowseHistory browseHistory)
-    {
-        return toAjax(browseHistoryCustomService.insertBrowseHistory(browseHistory));
+    public AjaxResult add(@RequestBody BrowseHistory browseHistory) {
+        return toAjax(browseHistoryService.save(browseHistory));
     }
 
     /**
@@ -81,9 +78,10 @@ public class BrowseHistoryController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_EDIT)
     @Log(title = "用户浏览历史", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody BrowseHistory browseHistory)
-    {
-        return toAjax(browseHistoryCustomService.updateBrowseHistory(browseHistory));
+    public AjaxResult edit(@RequestBody BrowseHistory browseHistory) {
+        LambdaQueryWrapper<BrowseHistory> qw = new LambdaQueryWrapper<>();
+        qw.eq(BrowseHistory::getBrowseHistoryId, browseHistory.getBrowseHistoryId());
+        return toAjax(browseHistoryService.update(browseHistory, qw));
     }
 
     /**
@@ -91,9 +89,9 @@ public class BrowseHistoryController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_BROWSE_HISTORY_REMOVE)
     @Log(title = "用户浏览历史", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{browseHistoryIds}")
-    public AjaxResult remove(@PathVariable Long[] browseHistoryIds)
-    {
-        return toAjax(browseHistoryCustomService.deleteBrowseHistoryByBrowseHistoryIds(browseHistoryIds));
+    @DeleteMapping("/{browseHistoryIds}")
+    public AjaxResult remove(@PathVariable Long[] browseHistoryIds) {
+        List<Long> idList = Arrays.asList(browseHistoryIds);
+        return toAjax(browseHistoryService.removeByIds(idList));
     }
 }
