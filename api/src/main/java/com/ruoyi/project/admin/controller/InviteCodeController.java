24751b2365c5f3949fd21e17aa92e38021db79f3
isinvon <2021469084@qq.com>
2025-02-01 03:39:45 +0800

    refactor(admin): 重构邀请码管理功能

--- api/src/main/java/com/ruoyi/project/admin/controller/InviteCodeController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/InviteCodeController.java
@@ -1,5 +1,6 @@
 package com.ruoyi.project.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
@@ -9,35 +10,34 @@ import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
 import com.ruoyi.framework.web.page.TableDataInfo;
 import com.ruoyi.project.admin.domain.InviteCode;
-import com.ruoyi.project.admin.service.custom.InviteCodeCustomService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ruoyi.project.admin.service.InviteCodeService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 邀请码管理Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/InviteCode")
-public class InviteCodeController extends BaseController
-{
-    @Autowired
-    private InviteCodeCustomService inviteCodeCustomService;
+public class InviteCodeController extends BaseController {
+    @Resource
+    private InviteCodeService inviteCodeService;
 
     /**
      * 查询邀请码管理列表
      */
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(InviteCode inviteCode)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<InviteCode> list = inviteCodeCustomService.selectInviteCodeList(inviteCode);
+        List<InviteCode> list = inviteCodeService.list();
         return getDataTable(list);
     }
 
@@ -47,10 +47,9 @@ public class InviteCodeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_EXPORT)
     @Log(title = "邀请码管理", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, InviteCode inviteCode)
-    {
-        List<InviteCode> list = inviteCodeCustomService.selectInviteCodeList(inviteCode);
-        ExcelUtil<InviteCode> util = new ExcelUtil<InviteCode>(InviteCode.class);
+    public void export(HttpServletResponse response) {
+        List<InviteCode> list = inviteCodeService.list();
+        ExcelUtil<InviteCode> util = new ExcelUtil<>(InviteCode.class);
         util.exportExcel(response, list, "邀请码管理数据");
     }
 
@@ -59,9 +58,8 @@ public class InviteCodeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_QUERY)
     @GetMapping(value = "/{inviteCodeId}")
-    public AjaxResult getInfo(@PathVariable("inviteCodeId") Long inviteCodeId)
-    {
-        return success(inviteCodeCustomService.selectInviteCodeByInviteCodeId(inviteCodeId));
+    public AjaxResult getInfo(@PathVariable("inviteCodeId") Long inviteCodeId) {
+        return success(inviteCodeService.getById(inviteCodeId));
     }
 
     /**
@@ -70,9 +68,8 @@ public class InviteCodeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_ADD)
     @Log(title = "邀请码管理", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody InviteCode inviteCode)
-    {
-        return toAjax(inviteCodeCustomService.insertInviteCode(inviteCode));
+    public AjaxResult add(@RequestBody InviteCode inviteCode) {
+        return toAjax(inviteCodeService.save(inviteCode));
     }
 
     /**
@@ -81,9 +78,10 @@ public class InviteCodeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_EDIT)
     @Log(title = "邀请码管理", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody InviteCode inviteCode)
-    {
-        return toAjax(inviteCodeCustomService.updateInviteCode(inviteCode));
+    public AjaxResult edit(@RequestBody InviteCode inviteCode) {
+        LambdaQueryWrapper<InviteCode> qw = new LambdaQueryWrapper<>();
+        qw.eq(InviteCode::getInviteCodeId, inviteCode.getInviteCodeId());
+        return toAjax(inviteCodeService.update(inviteCode, qw));
     }
 
     /**
@@ -91,9 +89,9 @@ public class InviteCodeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_INVITE_CODE_REMOVE)
     @Log(title = "邀请码管理", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{inviteCodeIds}")
-    public AjaxResult remove(@PathVariable Long[] inviteCodeIds)
-    {
-        return toAjax(inviteCodeCustomService.deleteInviteCodeByInviteCodeIds(inviteCodeIds));
+    @DeleteMapping("/{inviteCodeIds}")
+    public AjaxResult remove(@PathVariable Long[] inviteCodeIds) {
+        List<Long> idList = Arrays.asList(inviteCodeIds);
+        return toAjax(inviteCodeService.removeByIds(idList));
     }
 }
