b847ee9059bfa571597f138483b46992dc235108
isinvon <2021469084@qq.com>
2025-02-01 03:49:20 +0800

    refactor(admin): 重构通知用户关系控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/NoticeUserController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/NoticeUserController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.NoticeUser;
-import com.ruoyi.project.admin.service.custom.NoticeUserCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.NoticeUser;
+import com.ruoyi.project.admin.service.NoticeUserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 通知用户关系Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/noticeUser")
-public class NoticeUserController extends BaseController
-{
-    @Autowired
-    private NoticeUserCustomService noticeUserCustomService;
+public class NoticeUserController extends BaseController {
+    @Resource
+    private NoticeUserService noticeUserService;
 
     /**
      * 查询通知用户关系列表
      */
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(NoticeUser noticeUser)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<NoticeUser> list = noticeUserCustomService.selectNoticeUserList(noticeUser);
+        List<NoticeUser> list = noticeUserService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class NoticeUserController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_EXPORT)
     @Log(title = "通知用户关系", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, NoticeUser noticeUser)
-    {
-        List<NoticeUser> list = noticeUserCustomService.selectNoticeUserList(noticeUser);
-        ExcelUtil<NoticeUser> util = new ExcelUtil<NoticeUser>(NoticeUser.class);
+    public void export(HttpServletResponse response) {
+        List<NoticeUser> list = noticeUserService.list();
+        ExcelUtil<NoticeUser> util = new ExcelUtil<>(NoticeUser.class);
         util.exportExcel(response, list, "通知用户关系数据");
     }
 
@@ -66,9 +58,8 @@ public class NoticeUserController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_QUERY)
     @GetMapping(value = "/{relationId}")
-    public AjaxResult getInfo(@PathVariable("relationId") Long relationId)
-    {
-        return success(noticeUserCustomService.selectNoticeUserByRelationId(relationId));
+    public AjaxResult getInfo(@PathVariable("relationId") Long relationId) {
+        return success(noticeUserService.getById(relationId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class NoticeUserController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_ADD)
     @Log(title = "通知用户关系", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody NoticeUser noticeUser)
-    {
-        return toAjax(noticeUserCustomService.insertNoticeUser(noticeUser));
+    public AjaxResult add(@RequestBody NoticeUser noticeUser) {
+        return toAjax(noticeUserService.save(noticeUser));
     }
 
     /**
@@ -88,9 +78,10 @@ public class NoticeUserController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_EDIT)
     @Log(title = "通知用户关系", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody NoticeUser noticeUser)
-    {
-        return toAjax(noticeUserCustomService.updateNoticeUser(noticeUser));
+    public AjaxResult edit(@RequestBody NoticeUser noticeUser) {
+        LambdaQueryWrapper<NoticeUser> qw = new LambdaQueryWrapper<>();
+        qw.eq(NoticeUser::getRelationId, noticeUser.getRelationId());
+        return toAjax(noticeUserService.update(noticeUser, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class NoticeUserController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_NOTICE_USER_REMOVE)
     @Log(title = "通知用户关系", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{relationIds}")
-    public AjaxResult remove(@PathVariable Long[] relationIds)
-    {
-        return toAjax(noticeUserCustomService.deleteNoticeUserByRelationIds(relationIds));
+    @DeleteMapping("/{relationIds}")
+    public AjaxResult remove(@PathVariable Long[] relationIds) {
+        List<Long> idList = Arrays.asList(relationIds);
+        return toAjax(noticeUserService.removeByIds(idList));
     }
 }
