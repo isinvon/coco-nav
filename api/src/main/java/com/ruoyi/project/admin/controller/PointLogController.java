02e5963c7468d4c766de4d5f77aed7e06d8946da
isinvon <2021469084@qq.com>
2025-02-01 15:06:42 +0800

    refactor(admin): 重构积分日志控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/PointLogController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/PointLogController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.PointLog;
-import com.ruoyi.project.admin.service.custom.PointLogCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.PointLog;
+import com.ruoyi.project.admin.service.PointLogService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 积分流水Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/pointLog")
-public class PointLogController extends BaseController
-{
-    @Autowired
-    private PointLogCustomService pointLogCustomService;
+public class PointLogController extends BaseController {
+    @Resource
+    private PointLogService pointLogService;
 
     /**
      * 查询积分流水列表
      */
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(PointLog pointLog)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<PointLog> list = pointLogCustomService.selectPointLogList(pointLog);
+        List<PointLog> list = pointLogService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class PointLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_EXPORT)
     @Log(title = "积分流水", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, PointLog pointLog)
-    {
-        List<PointLog> list = pointLogCustomService.selectPointLogList(pointLog);
-        ExcelUtil<PointLog> util = new ExcelUtil<PointLog>(PointLog.class);
+    public void export(HttpServletResponse response) {
+        List<PointLog> list = pointLogService.list();
+        ExcelUtil<PointLog> util = new ExcelUtil<>(PointLog.class);
         util.exportExcel(response, list, "积分流水数据");
     }
 
@@ -66,9 +58,8 @@ public class PointLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_QUERY)
     @GetMapping(value = "/{pointLogId}")
-    public AjaxResult getInfo(@PathVariable("pointLogId") Long pointLogId)
-    {
-        return success(pointLogCustomService.selectPointLogByPointLogId(pointLogId));
+    public AjaxResult getInfo(@PathVariable("pointLogId") Long pointLogId) {
+        return success(pointLogService.getById(pointLogId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class PointLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_ADD)
     @Log(title = "积分流水", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody PointLog pointLog)
-    {
-        return toAjax(pointLogCustomService.insertPointLog(pointLog));
+    public AjaxResult add(@RequestBody PointLog pointLog) {
+        return toAjax(pointLogService.save(pointLog));
     }
 
     /**
@@ -88,9 +78,10 @@ public class PointLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_EDIT)
     @Log(title = "积分流水", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody PointLog pointLog)
-    {
-        return toAjax(pointLogCustomService.updatePointLog(pointLog));
+    public AjaxResult edit(@RequestBody PointLog pointLog) {
+        LambdaQueryWrapper<PointLog> qw = new LambdaQueryWrapper<>();
+        qw.eq(PointLog::getPointLogId, pointLog.getPointLogId());
+        return toAjax(pointLogService.update(pointLog, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class PointLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_POINT_LOG_REMOVE)
     @Log(title = "积分流水", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{pointLogIds}")
-    public AjaxResult remove(@PathVariable Long[] pointLogIds)
-    {
-        return toAjax(pointLogCustomService.deletePointLogByPointLogIds(pointLogIds));
+    @DeleteMapping("/{pointLogIds}")
+    public AjaxResult remove(@PathVariable Long[] pointLogIds) {
+        List<Long> idList = Arrays.asList(pointLogIds);
+        return toAjax(pointLogService.removeByIds(idList));
     }
 }
