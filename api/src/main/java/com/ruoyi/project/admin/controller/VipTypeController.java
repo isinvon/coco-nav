9601af1eb928c7d407085cadf02fe3494f68681d
isinvon <2021469084@qq.com>
2025-02-01 15:29:56 +0800

    refactor(admin): 重构vip会员类型控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/VipTypeController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/VipTypeController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.VipType;
-import com.ruoyi.project.admin.service.custom.VipTypeCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.VipType;
+import com.ruoyi.project.admin.service.VipTypeService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * VIP类型Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/vipType")
-public class VipTypeController extends BaseController
-{
-    @Autowired
-    private VipTypeCustomService vipTypeCustomService;
+public class VipTypeController extends BaseController {
+    @Resource
+    private VipTypeService vipTypeService;
 
     /**
      * 查询VIP类型列表
      */
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(VipType vipType)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<VipType> list = vipTypeCustomService.selectVipTypeList(vipType);
+        List<VipType> list = vipTypeService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class VipTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_EXPORT)
     @Log(title = "VIP类型", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, VipType vipType)
-    {
-        List<VipType> list = vipTypeCustomService.selectVipTypeList(vipType);
-        ExcelUtil<VipType> util = new ExcelUtil<VipType>(VipType.class);
+    public void export(HttpServletResponse response) {
+        List<VipType> list = vipTypeService.list();
+        ExcelUtil<VipType> util = new ExcelUtil<>(VipType.class);
         util.exportExcel(response, list, "VIP类型数据");
     }
 
@@ -66,9 +58,8 @@ public class VipTypeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_QUERY)
     @GetMapping(value = "/{vipTypeId}")
-    public AjaxResult getInfo(@PathVariable("vipTypeId") Long vipTypeId)
-    {
-        return success(vipTypeCustomService.selectVipTypeByVipTypeId(vipTypeId));
+    public AjaxResult getInfo(@PathVariable("vipTypeId") Long vipTypeId) {
+        return success(vipTypeService.getById(vipTypeId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class VipTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_ADD)
     @Log(title = "VIP类型", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody VipType vipType)
-    {
-        return toAjax(vipTypeCustomService.insertVipType(vipType));
+    public AjaxResult add(@RequestBody VipType vipType) {
+        return toAjax(vipTypeService.save(vipType));
     }
 
     /**
@@ -88,9 +78,10 @@ public class VipTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_EDIT)
     @Log(title = "VIP类型", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody VipType vipType)
-    {
-        return toAjax(vipTypeCustomService.updateVipType(vipType));
+    public AjaxResult edit(@RequestBody VipType vipType) {
+        LambdaUpdateWrapper<VipType> uw = new LambdaUpdateWrapper<>();
+        uw.eq(VipType::getVipTypeId, vipType.getVipTypeId());
+        return toAjax(vipTypeService.update(vipType, uw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class VipTypeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_VIP_TYPE_REMOVE)
     @Log(title = "VIP类型", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{vipTypeIds}")
-    public AjaxResult remove(@PathVariable Long[] vipTypeIds)
-    {
-        return toAjax(vipTypeCustomService.deleteVipTypeByVipTypeIds(vipTypeIds));
+    @DeleteMapping("/{vipTypeIds}")
+    public AjaxResult remove(@PathVariable Long[] vipTypeIds) {
+        List<Long> idList = Arrays.asList(vipTypeIds);
+        return toAjax(vipTypeService.removeByIds(idList));
     }
 }
