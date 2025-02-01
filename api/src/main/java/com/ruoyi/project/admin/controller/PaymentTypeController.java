15eee0353d06ce9db8a4e9922ddbd4fe6a4c13e3
isinvon <2021469084@qq.com>
2025-02-01 15:00:14 +0800

    refactor(admin): 重构支付类型控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/PaymentTypeController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/PaymentTypeController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.PaymentType;
-import com.ruoyi.project.admin.service.custom.PaymentTypeCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.PaymentType;
+import com.ruoyi.project.admin.service.PaymentTypeService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 支付方式Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/paymentType")
-public class PaymentTypeController extends BaseController
-{
-    @Autowired
-    private PaymentTypeCustomService paymentTypeCustomService;
+public class PaymentTypeController extends BaseController {
+    @Resource
+    private PaymentTypeService paymentTypeService;
 
     /**
      * 查询支付方式列表
      */
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(PaymentType paymentType)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<PaymentType> list = paymentTypeCustomService.selectPaymentTypeList(paymentType);
+        List<PaymentType> list = paymentTypeService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class PaymentTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_EXPORT)
     @Log(title = "支付方式", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, PaymentType paymentType)
-    {
-        List<PaymentType> list = paymentTypeCustomService.selectPaymentTypeList(paymentType);
-        ExcelUtil<PaymentType> util = new ExcelUtil<PaymentType>(PaymentType.class);
+    public void export(HttpServletResponse response) {
+        List<PaymentType> list = paymentTypeService.list();
+        ExcelUtil<PaymentType> util = new ExcelUtil<>(PaymentType.class);
         util.exportExcel(response, list, "支付方式数据");
     }
 
@@ -66,9 +58,8 @@ public class PaymentTypeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_QUERY)
     @GetMapping(value = "/{paymentTypeId}")
-    public AjaxResult getInfo(@PathVariable("paymentTypeId") Long paymentTypeId)
-    {
-        return success(paymentTypeCustomService.selectPaymentTypeByPaymentTypeId(paymentTypeId));
+    public AjaxResult getInfo(@PathVariable("paymentTypeId") Long paymentTypeId) {
+        return success(paymentTypeService.getById(paymentTypeId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class PaymentTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_ADD)
     @Log(title = "支付方式", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody PaymentType paymentType)
-    {
-        return toAjax(paymentTypeCustomService.insertPaymentType(paymentType));
+    public AjaxResult add(@RequestBody PaymentType paymentType) {
+        return toAjax(paymentTypeService.save(paymentType));
     }
 
     /**
@@ -88,9 +78,10 @@ public class PaymentTypeController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_EDIT)
     @Log(title = "支付方式", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody PaymentType paymentType)
-    {
-        return toAjax(paymentTypeCustomService.updatePaymentType(paymentType));
+    public AjaxResult edit(@RequestBody PaymentType paymentType) {
+        LambdaQueryWrapper<PaymentType> qw = new LambdaQueryWrapper<>();
+        qw.eq(PaymentType::getPaymentTypeId, paymentType.getPaymentTypeId());
+        return toAjax(paymentTypeService.update(paymentType, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class PaymentTypeController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_PAYMENT_TYPE_REMOVE)
     @Log(title = "支付方式", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{paymentTypeIds}")
-    public AjaxResult remove(@PathVariable Long[] paymentTypeIds)
-    {
-        return toAjax(paymentTypeCustomService.deletePaymentTypeByPaymentTypeIds(paymentTypeIds));
+    @DeleteMapping("/{paymentTypeIds}")
+    public AjaxResult remove(@PathVariable Long[] paymentTypeIds) {
+        List<Long> idList = Arrays.asList(paymentTypeIds);
+        return toAjax(paymentTypeService.removeByIds(idList));
     }
 }
