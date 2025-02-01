a7df0bd395d86798601a8dfa7a57499c58460c87
isinvon <2021469084@qq.com>
2025-02-01 15:32:19 +0800

    refactor(admin): 重构网站信息控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/WebsiteInfoController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/WebsiteInfoController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.WebsiteInfo;
-import com.ruoyi.project.admin.service.custom.WebsiteInfoCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.WebsiteInfo;
+import com.ruoyi.project.admin.service.WebsiteInfoService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 网站信息Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/websiteInfo")
-public class WebsiteInfoController extends BaseController
-{
-    @Autowired
-    private WebsiteInfoCustomService websiteInfoCustomService;
+public class WebsiteInfoController extends BaseController {
+    @Resource
+    private WebsiteInfoService websiteInfoService;
 
     /**
      * 查询网站信息列表
      */
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(WebsiteInfo websiteInfo)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<WebsiteInfo> list = websiteInfoCustomService.selectWebsiteInfoList(websiteInfo);
+        List<WebsiteInfo> list = websiteInfoService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class WebsiteInfoController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_EXPORT)
     @Log(title = "网站信息", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, WebsiteInfo websiteInfo)
-    {
-        List<WebsiteInfo> list = websiteInfoCustomService.selectWebsiteInfoList(websiteInfo);
-        ExcelUtil<WebsiteInfo> util = new ExcelUtil<WebsiteInfo>(WebsiteInfo.class);
+    public void export(HttpServletResponse response) {
+        List<WebsiteInfo> list = websiteInfoService.list();
+        ExcelUtil<WebsiteInfo> util = new ExcelUtil<>(WebsiteInfo.class);
         util.exportExcel(response, list, "网站信息数据");
     }
 
@@ -66,9 +58,8 @@ public class WebsiteInfoController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_QUERY)
     @GetMapping(value = "/{websiteInfoId}")
-    public AjaxResult getInfo(@PathVariable("websiteInfoId") Long websiteInfoId)
-    {
-        return success(websiteInfoCustomService.selectWebsiteInfoByWebsiteInfoId(websiteInfoId));
+    public AjaxResult getInfo(@PathVariable("websiteInfoId") Long websiteInfoId) {
+        return success(websiteInfoService.getById(websiteInfoId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class WebsiteInfoController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_ADD)
     @Log(title = "网站信息", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody WebsiteInfo websiteInfo)
-    {
-        return toAjax(websiteInfoCustomService.insertWebsiteInfo(websiteInfo));
+    public AjaxResult add(@RequestBody WebsiteInfo websiteInfo) {
+        return toAjax(websiteInfoService.save(websiteInfo));
     }
 
     /**
@@ -88,9 +78,9 @@ public class WebsiteInfoController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_EDIT)
     @Log(title = "网站信息", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody WebsiteInfo websiteInfo)
-    {
-        return toAjax(websiteInfoCustomService.updateWebsiteInfo(websiteInfo));
+    public AjaxResult edit(@RequestBody WebsiteInfo websiteInfo) {
+        LambdaUpdateWrapper<WebsiteInfo> uw = new LambdaUpdateWrapper<>();
+        return toAjax(websiteInfoService.update(websiteInfo, uw));
     }
 
     /**
@@ -98,9 +88,9 @@ public class WebsiteInfoController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_WEBSITE_INFO_REMOVE)
     @Log(title = "网站信息", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{websiteInfoIds}")
-    public AjaxResult remove(@PathVariable Long[] websiteInfoIds)
-    {
-        return toAjax(websiteInfoCustomService.deleteWebsiteInfoByWebsiteInfoIds(websiteInfoIds));
+    @DeleteMapping("/{websiteInfoIds}")
+    public AjaxResult remove(@PathVariable Long[] websiteInfoIds) {
+        List<Long> idList = Arrays.asList(websiteInfoIds);
+        return toAjax(websiteInfoService.removeByIds(idList));
     }
 }
