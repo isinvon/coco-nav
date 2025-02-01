7bfe00042f069c8ad6dcf73a0a2ae34d5e46fd1a
isinvon <2021469084@qq.com>
2025-02-01 15:21:14 +0800

    refactor(admin): 重构用户登录日志控制器

--- api/src/main/java/com/ruoyi/project/admin/controller/UserLoginLogController.java
+++ api/src/main/java/com/ruoyi/project/admin/controller/UserLoginLogController.java
@@ -1,50 +1,43 @@
 package com.ruoyi.project.admin.controller;
 
-import java.util.List;
-import javax.servlet.http.HttpServletResponse;
-
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.ruoyi.common.constant.PermissionConstants;
-import com.ruoyi.framework.security.permission.CustomPermission;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
-import com.ruoyi.project.admin.domain.UserLoginLog;
-import com.ruoyi.project.admin.service.custom.UserLoginLogCustomService;
+import com.ruoyi.framework.security.permission.CustomPermission;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
-import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.project.admin.domain.UserLoginLog;
+import com.ruoyi.project.admin.service.UserLoginLogService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 用户登录日志Controller
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @RestController
 @RequestMapping("/admin/userLoginLog")
-public class UserLoginLogController extends BaseController
-{
-    @Autowired
-    private UserLoginLogCustomService userLoginLogCustomService;
+public class UserLoginLogController extends BaseController {
+    @Resource
+    private UserLoginLogService userLoginLogService;
 
     /**
      * 查询用户登录日志列表
      */
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_LIST)
     @GetMapping("/list")
-    public TableDataInfo list(UserLoginLog userLoginLog)
-    {
+    public TableDataInfo list() {
         startPage();
-        List<UserLoginLog> list = userLoginLogCustomService.selectUserLoginLogList(userLoginLog);
+        List<UserLoginLog> list = userLoginLogService.list();
         return getDataTable(list);
     }
 
@@ -54,10 +47,9 @@ public class UserLoginLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_EXPORT)
     @Log(title = "用户登录日志", businessType = BusinessType.EXPORT)
     @PostMapping("/export")
-    public void export(HttpServletResponse response, UserLoginLog userLoginLog)
-    {
-        List<UserLoginLog> list = userLoginLogCustomService.selectUserLoginLogList(userLoginLog);
-        ExcelUtil<UserLoginLog> util = new ExcelUtil<UserLoginLog>(UserLoginLog.class);
+    public void export(HttpServletResponse response) {
+        List<UserLoginLog> list = userLoginLogService.list();
+        ExcelUtil<UserLoginLog> util = new ExcelUtil<>(UserLoginLog.class);
         util.exportExcel(response, list, "用户登录日志数据");
     }
 
@@ -66,9 +58,8 @@ public class UserLoginLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_QUERY)
     @GetMapping(value = "/{userLoginLogId}")
-    public AjaxResult getInfo(@PathVariable("userLoginLogId") Long userLoginLogId)
-    {
-        return success(userLoginLogCustomService.selectUserLoginLogByUserLoginLogId(userLoginLogId));
+    public AjaxResult getInfo(@PathVariable("userLoginLogId") Long userLoginLogId) {
+        return success(userLoginLogService.getById(userLoginLogId));
     }
 
     /**
@@ -77,9 +68,8 @@ public class UserLoginLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_ADD)
     @Log(title = "用户登录日志", businessType = BusinessType.INSERT)
     @PostMapping
-    public AjaxResult add(@RequestBody UserLoginLog userLoginLog)
-    {
-        return toAjax(userLoginLogCustomService.insertUserLoginLog(userLoginLog));
+    public AjaxResult add(@RequestBody UserLoginLog userLoginLog) {
+        return toAjax(userLoginLogService.save(userLoginLog));
     }
 
     /**
@@ -88,9 +78,10 @@ public class UserLoginLogController extends BaseController
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_EDIT)
     @Log(title = "用户登录日志", businessType = BusinessType.UPDATE)
     @PutMapping
-    public AjaxResult edit(@RequestBody UserLoginLog userLoginLog)
-    {
-        return toAjax(userLoginLogCustomService.updateUserLoginLog(userLoginLog));
+    public AjaxResult edit(@RequestBody UserLoginLog userLoginLog) {
+        LambdaQueryWrapper<UserLoginLog> qw = new LambdaQueryWrapper<>();
+        qw.eq(UserLoginLog::getUserLoginLogId, userLoginLog.getUserLoginLogId());
+        return toAjax(userLoginLogService.update(userLoginLog, qw));
     }
 
     /**
@@ -98,9 +89,9 @@ public class UserLoginLogController extends BaseController
      */
     @CustomPermission(PermissionConstants.ADMIN_USER_LOGIN_LOG_REMOVE)
     @Log(title = "用户登录日志", businessType = BusinessType.DELETE)
-	@DeleteMapping("/{userLoginLogIds}")
-    public AjaxResult remove(@PathVariable Long[] userLoginLogIds)
-    {
-        return toAjax(userLoginLogCustomService.deleteUserLoginLogByUserLoginLogIds(userLoginLogIds));
+    @DeleteMapping("/{userLoginLogIds}")
+    public AjaxResult remove(@PathVariable Long[] userLoginLogIds) {
+        List<Long> idList = Arrays.asList(userLoginLogIds);
+        return toAjax(userLoginLogService.removeByIds(idList));
     }
 }
