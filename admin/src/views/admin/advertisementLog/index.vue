52a65640e7ea436648bb1090cb3216a6a88d885c
isinvon <2021469084@qq.com>
2025-02-01 19:44:49 +0800

    refactor: 重构广告日志页面

--- admin/src/views/admin/advertisementLog/index.vue
+++ admin/src/views/admin/advertisementLog/index.vue
@@ -9,6 +9,7 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
+
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
         <el-button icon="Refresh" @click="resetQuery">重置</el-button>
@@ -16,35 +17,6 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['admin:advertisementLog:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['admin:advertisementLog:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['admin:advertisementLog:remove']"
-        >删除</el-button>
-      </el-col>
       <el-col :span="1.5">
         <el-button
           type="warning"
@@ -57,18 +29,11 @@
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="advertisementLogList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
+    <el-table v-loading="loading" :data="advertisementLogList">
       <el-table-column label="日志ID" align="center" prop="id" />
       <el-table-column label="广告ID" align="center" prop="advertisementId" />
       <el-table-column label="操作类型" align="center" prop="action" />
       <el-table-column label="操作人ID" align="center" prop="operatorId" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-        <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['admin:advertisementLog:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['admin:advertisementLog:remove']">删除</el-button>
-        </template>
-      </el-table-column>
     </el-table>
     
     <pagination
@@ -78,21 +43,6 @@
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-
-    <!-- 添加或修改广告操作日志对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="advertisementLogRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="操作类型" prop="action">
-          <el-input v-model="form.action" placeholder="请输入操作类型" />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
-          <el-button @click="cancel">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
   </div>
 </template>
 
@@ -120,23 +70,6 @@ const data = reactive({
     action: null,
     operatorId: null,
   },
-  rules: {
-    advertisementId: [
-      { required: true, message: "广告ID不能为空", trigger: "blur" }
-    ],
-    action: [
-      { required: true, message: "操作类型不能为空", trigger: "blur" }
-    ],
-    operatorId: [
-      { required: true, message: "操作人ID不能为空", trigger: "blur" }
-    ],
-    createTime: [
-      { required: true, message: "创建时间不能为空", trigger: "blur" }
-    ],
-    updateTime: [
-      { required: true, message: "更新时间不能为空", trigger: "blur" }
-    ]
-  }
 });
 
 const { queryParams, form, rules } = toRefs(data);
@@ -151,25 +84,6 @@ function getList() {
   });
 }
 
-// 取消按钮
-function cancel() {
-  open.value = false;
-  reset();
-}
-
-// 表单重置
-function reset() {
-  form.value = {
-    id: null,
-    advertisementId: null,
-    action: null,
-    operatorId: null,
-    createTime: null,
-    updateTime: null
-  };
-  proxy.resetForm("advertisementLogRef");
-}
-
 /** 搜索按钮操作 */
 function handleQuery() {
   queryParams.value.pageNum = 1;
@@ -180,63 +94,6 @@ function handleQuery() {
 function resetQuery() {
   proxy.resetForm("queryRef");
   handleQuery();
-}
-
-// 多选框选中数据
-function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.id);
-  single.value = selection.length != 1;
-  multiple.value = !selection.length;
-}
-
-/** 新增按钮操作 */
-function handleAdd() {
-  reset();
-  open.value = true;
-  title.value = "添加广告操作日志";
-}
-
-/** 修改按钮操作 */
-function handleUpdate(row) {
-  reset();
-  const _id = row.id || ids.value
-  getAdvertisementLog(_id).then(response => {
-    form.value = response.data;
-    open.value = true;
-    title.value = "修改广告操作日志";
-  });
-}
-
-/** 提交按钮 */
-function submitForm() {
-  proxy.$refs["advertisementLogRef"].validate(valid => {
-    if (valid) {
-      if (form.value.id != null) {
-        updateAdvertisementLog(form.value).then(response => {
-          proxy.$modal.msgSuccess("修改成功");
-          open.value = false;
-          getList();
-        });
-      } else {
-        addAdvertisementLog(form.value).then(response => {
-          proxy.$modal.msgSuccess("新增成功");
-          open.value = false;
-          getList();
-        });
-      }
-    }
-  });
-}
-
-/** 删除按钮操作 */
-function handleDelete(row) {
-  const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除广告操作日志编号为"' + _ids + '"的数据项？').then(function() {
-    return delAdvertisementLog(_ids);
-  }).then(() => {
-    getList();
-    proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
 }
 
 /** 导出按钮操作 */
