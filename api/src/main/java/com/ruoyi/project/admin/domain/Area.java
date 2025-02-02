9c1023a4a9d5c399b3f9bdd4e4e70759975fe7cb
isinvon <2021469084@qq.com>
2025-02-02 22:48:24 +0800

    refactor(admin): 重构 Area 类
    - 添加实现 Serializable 接口以支持序列化
    - 明确 id 字段为地区ID，并添加 Excel 注解
    - 将 parentId 字段注释修改为父级ID
    - 将 level 字段改名为 depth，以更准确地表示行政级别
    - 移除 code 字段，增加 sort 字段用于地区排序

--- api/src/main/java/com/ruoyi/project/admin/domain/Area.java
+++ api/src/main/java/com/ruoyi/project/admin/domain/Area.java
@@ -7,31 +7,38 @@ import lombok.Data;
 
 import com.ruoyi.framework.aspectj.lang.annotation.Excel;
 
+import java.io.Serializable;
+
 /**
  * 地区信息对象 coco_area
- * 
+ * 在 coco_area 表中地区编号就是 “id” 字段
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Data
 @TableName("coco_area")
-public class Area extends BaseEntity
+public class Area implements Serializable
 {
     private static final long serialVersionUID = 1L;
+
+    /** 地区ID */
+    @Excel(name = "地区ID")
+    private Integer id;
 
     /** 地区名称 */
     @Excel(name = "地区名称")
     private String name;
 
-    /** 上级地区ID */
-    @Excel(name = "上级地区ID")
+    /** 父级ID */
+    @Excel(name = "父级ID")
     private Long parentId;
 
     /** 行政级别：1-省 2-市 3-区 */
     @Excel(name = "行政级别：1-省 2-市 3-区")
-    private Integer level;
+    private Integer depth;
 
-    /** 行政编码 */
-    @Excel(name = "行政编码")
-    private String code;
+    /** 地区排序 */
+    @Excel(name = "地区排序")
+    private String sort;
 }
