84a40a457cf91e83378e77e8a277e4772df1ac8c
isinvon <2021469084@qq.com>
2025-02-02 22:49:26 +0800

    refactor(admin): 重构地区信息接口和实现类
    - 移除了 AreaMapper 接口中的所有自定义方法
    - 删除了 AreaServiceImpl 类中的所有自定义方法

--- api/src/main/java/com/ruoyi/project/admin/mapper/AreaMapper.java
+++ api/src/main/java/com/ruoyi/project/admin/mapper/AreaMapper.java
@@ -1,64 +1,18 @@
 package com.ruoyi.project.admin.mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import java.util.List;
+
 import com.ruoyi.project.admin.domain.Area;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 地区信息Mapper接口
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Mapper
-public interface AreaMapper extends BaseMapper<Area>
-{
-    /**
-     * 查询地区信息
-     * 
-     * @param areaId 地区信息主键
-     * @return 地区信息
-     */
-    public Area selectAreaByAreaId(Long areaId);
-
-    /**
-     * 查询地区信息列表
-     * 
-     * @param area 地区信息
-     * @return 地区信息集合
-     */
-    public List<Area> selectAreaList(Area area);
-
-    /**
-     * 新增地区信息
-     * 
-     * @param area 地区信息
-     * @return 结果
-     */
-    public int insertArea(Area area);
-
-    /**
-     * 修改地区信息
-     * 
-     * @param area 地区信息
-     * @return 结果
-     */
-    public int updateArea(Area area);
-
-    /**
-     * 删除地区信息
-     * 
-     * @param areaId 地区信息主键
-     * @return 结果
-     */
-    public int deleteAreaByAreaId(Long areaId);
-
-    /**
-     * 批量删除地区信息
-     * 
-     * @param areaIds 需要删除的数据主键集合
-     * @return 结果
-     */
-    public int deleteAreaByAreaIds(Long[] areaIds);
+public interface AreaMapper extends BaseMapper<Area> {
 }
--- api/src/main/java/com/ruoyi/project/admin/service/impl/AreaServiceImpl.java
+++ api/src/main/java/com/ruoyi/project/admin/service/impl/AreaServiceImpl.java
@@ -1,98 +1,18 @@
 package com.ruoyi.project.admin.service.impl;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.ruoyi.project.admin.service.AreaService;
 
-import java.util.List;
-import com.ruoyi.common.utils.DateUtils;
-import com.ruoyi.project.admin.service.custom.AreaCustomService;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.ruoyi.project.admin.mapper.AreaMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.project.admin.domain.Area;
+import com.ruoyi.project.admin.mapper.AreaMapper;
+import com.ruoyi.project.admin.service.AreaService;
 import org.springframework.stereotype.Service;
 
 /**
  * 地区信息Service业务层处理
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Service
-public class AreaServiceImpl extends ServiceImpl<AreaMapper,Area> implements AreaCustomService, AreaService
-{
-    @Autowired
-    private AreaMapper areaMapper;
+public class AreaServiceImpl extends ServiceImpl<AreaMapper, Area> implements AreaService {
 
-    /**
-     * 查询地区信息
-     * 
-     * @param areaId 地区信息主键
-     * @return 地区信息
-     */
-    @Override
-    public Area selectAreaByAreaId(Long areaId)
-    {
-        return areaMapper.selectAreaByAreaId(areaId);
-    }
-
-    /**
-     * 查询地区信息列表
-     * 
-     * @param area 地区信息
-     * @return 地区信息
-     */
-    @Override
-    public List<Area> selectAreaList(Area area)
-    {
-        return areaMapper.selectAreaList(area);
-    }
-
-    /**
-     * 新增地区信息
-     * 
-     * @param area 地区信息
-     * @return 结果
-     */
-    @Override
-    public int insertArea(Area area)
-    {
-        area.setCreateTime(DateUtils.getNowDate());
-        return areaMapper.insertArea(area);
-    }
-
-    /**
-     * 修改地区信息
-     * 
-     * @param area 地区信息
-     * @return 结果
-     */
-    @Override
-    public int updateArea(Area area)
-    {
-        area.setUpdateTime(DateUtils.getNowDate());
-        return areaMapper.updateArea(area);
-    }
-
-    /**
-     * 批量删除地区信息
-     * 
-     * @param areaIds 需要删除的地区信息主键
-     * @return 结果
-     */
-    @Override
-    public int deleteAreaByAreaIds(Long[] areaIds)
-    {
-        return areaMapper.deleteAreaByAreaIds(areaIds);
-    }
-
-    /**
-     * 删除地区信息信息
-     * 
-     * @param areaId 地区信息主键
-     * @return 结果
-     */
-    @Override
-    public int deleteAreaByAreaId(Long areaId)
-    {
-        return areaMapper.deleteAreaByAreaId(areaId);
-    }
 }
