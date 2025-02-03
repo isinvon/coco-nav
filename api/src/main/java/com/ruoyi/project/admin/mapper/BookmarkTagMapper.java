2e9b0cf5b537336442c780f061f5f9dd132209ad
isinvon <2021469084@qq.com>
2025-02-04 00:19:02 +0800

    refactor(admin): 重构书签标签模块
    - 删除了 BookmarkTagMapper 中的冗余方法
    - 移除了 BookmarkTagServiceImpl 中的未使用的代码
    - 简化了接口和实现类的结构

--- api/src/main/java/com/ruoyi/project/admin/mapper/BookmarkTagMapper.java
+++ api/src/main/java/com/ruoyi/project/admin/mapper/BookmarkTagMapper.java
@@ -1,64 +1,18 @@
 package com.ruoyi.project.admin.mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import java.util.List;
+
 import com.ruoyi.project.admin.domain.po.BookmarkTag;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 书签标签Mapper接口
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Mapper
-public interface BookmarkTagMapper extends BaseMapper<BookmarkTag>
-{
-    /**
-     * 查询书签标签
-     * 
-     * @param bookmarkTagId 书签标签主键
-     * @return 书签标签
-     */
-    public BookmarkTag selectBookmarkTagByBookmarkTagId(Long bookmarkTagId);
-
-    /**
-     * 查询书签标签列表
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 书签标签集合
-     */
-    public List<BookmarkTag> selectBookmarkTagList(BookmarkTag bookmarkTag);
-
-    /**
-     * 新增书签标签
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 结果
-     */
-    public int insertBookmarkTag(BookmarkTag bookmarkTag);
-
-    /**
-     * 修改书签标签
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 结果
-     */
-    public int updateBookmarkTag(BookmarkTag bookmarkTag);
-
-    /**
-     * 删除书签标签
-     * 
-     * @param bookmarkTagId 书签标签主键
-     * @return 结果
-     */
-    public int deleteBookmarkTagByBookmarkTagId(Long bookmarkTagId);
-
-    /**
-     * 批量删除书签标签
-     * 
-     * @param bookmarkTagIds 需要删除的数据主键集合
-     * @return 结果
-     */
-    public int deleteBookmarkTagByBookmarkTagIds(Long[] bookmarkTagIds);
+public interface BookmarkTagMapper extends BaseMapper<BookmarkTag> {
 }
--- api/src/main/java/com/ruoyi/project/admin/service/impl/BookmarkTagServiceImpl.java
+++ api/src/main/java/com/ruoyi/project/admin/service/impl/BookmarkTagServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ruoyi.project.admin.service.impl;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.project.admin.service.BookmarkTagService;
 
 import java.util.List;
+
 import com.ruoyi.common.utils.DateUtils;
 import com.ruoyi.project.admin.service.custom.BookmarkTagCustomService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,87 +15,11 @@ import org.springframework.stereotype.Service;
 
 /**
  * 书签标签Service业务层处理
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Service
-public class BookmarkTagServiceImpl extends ServiceImpl<BookmarkTagMapper,BookmarkTag> implements BookmarkTagCustomService, BookmarkTagService
-{
-    @Autowired
-    private BookmarkTagMapper bookmarkTagMapper;
+public class BookmarkTagServiceImpl extends ServiceImpl<BookmarkTagMapper, BookmarkTag> implements BookmarkTagService {
 
-    /**
-     * 查询书签标签
-     * 
-     * @param bookmarkTagId 书签标签主键
-     * @return 书签标签
-     */
-    @Override
-    public BookmarkTag selectBookmarkTagByBookmarkTagId(Long bookmarkTagId)
-    {
-        return bookmarkTagMapper.selectBookmarkTagByBookmarkTagId(bookmarkTagId);
-    }
-
-    /**
-     * 查询书签标签列表
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 书签标签
-     */
-    @Override
-    public List<BookmarkTag> selectBookmarkTagList(BookmarkTag bookmarkTag)
-    {
-        return bookmarkTagMapper.selectBookmarkTagList(bookmarkTag);
-    }
-
-    /**
-     * 新增书签标签
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 结果
-     */
-    @Override
-    public int insertBookmarkTag(BookmarkTag bookmarkTag)
-    {
-        bookmarkTag.setCreateTime(DateUtils.getNowDate());
-        return bookmarkTagMapper.insertBookmarkTag(bookmarkTag);
-    }
-
-    /**
-     * 修改书签标签
-     * 
-     * @param bookmarkTag 书签标签
-     * @return 结果
-     */
-    @Override
-    public int updateBookmarkTag(BookmarkTag bookmarkTag)
-    {
-        bookmarkTag.setUpdateTime(DateUtils.getNowDate());
-        return bookmarkTagMapper.updateBookmarkTag(bookmarkTag);
-    }
-
-    /**
-     * 批量删除书签标签
-     * 
-     * @param bookmarkTagIds 需要删除的书签标签主键
-     * @return 结果
-     */
-    @Override
-    public int deleteBookmarkTagByBookmarkTagIds(Long[] bookmarkTagIds)
-    {
-        return bookmarkTagMapper.deleteBookmarkTagByBookmarkTagIds(bookmarkTagIds);
-    }
-
-    /**
-     * 删除书签标签信息
-     * 
-     * @param bookmarkTagId 书签标签主键
-     * @return 结果
-     */
-    @Override
-    public int deleteBookmarkTagByBookmarkTagId(Long bookmarkTagId)
-    {
-        return bookmarkTagMapper.deleteBookmarkTagByBookmarkTagId(bookmarkTagId);
-    }
 }
