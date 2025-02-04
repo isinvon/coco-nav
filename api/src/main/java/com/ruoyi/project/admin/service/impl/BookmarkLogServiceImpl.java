44b912d7d3b298969f3da8773730898568ada4a2
isinvon <2021469084@qq.com>
2025-02-04 23:18:29 +0800

    refactor(admin): 重构书签操作日志模块
    - 移除了 BookmarkLogMapper 接口中的冗余方法
    - 删除了 BookmarkLogServiceImpl 类中的冗余实现- 简化了代码结构，提高了代码可维护性

--- api/src/main/java/com/ruoyi/project/admin/service/impl/BookmarkLogServiceImpl.java
+++ api/src/main/java/com/ruoyi/project/admin/service/impl/BookmarkLogServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ruoyi.project.admin.service.impl;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.project.admin.service.BookmarkLogService;
 
 import java.util.List;
+
 import com.ruoyi.common.utils.DateUtils;
 import com.ruoyi.project.admin.service.custom.BookmarkLogCustomService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,87 +15,10 @@ import org.springframework.stereotype.Service;
 
 /**
  * 书签操作日志Service业务层处理
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Service
-public class BookmarkLogServiceImpl extends ServiceImpl<BookmarkLogMapper,BookmarkLog> implements BookmarkLogCustomService, BookmarkLogService
-{
-    @Autowired
-    private BookmarkLogMapper bookmarkLogMapper;
-
-    /**
-     * 查询书签操作日志
-     * 
-     * @param bookmarkLogId 书签操作日志主键
-     * @return 书签操作日志
-     */
-    @Override
-    public BookmarkLog selectBookmarkLogByBookmarkLogId(Long bookmarkLogId)
-    {
-        return bookmarkLogMapper.selectBookmarkLogByBookmarkLogId(bookmarkLogId);
-    }
-
-    /**
-     * 查询书签操作日志列表
-     * 
-     * @param bookmarkLog 书签操作日志
-     * @return 书签操作日志
-     */
-    @Override
-    public List<BookmarkLog> selectBookmarkLogList(BookmarkLog bookmarkLog)
-    {
-        return bookmarkLogMapper.selectBookmarkLogList(bookmarkLog);
-    }
-
-    /**
-     * 新增书签操作日志
-     * 
-     * @param bookmarkLog 书签操作日志
-     * @return 结果
-     */
-    @Override
-    public int insertBookmarkLog(BookmarkLog bookmarkLog)
-    {
-        bookmarkLog.setCreateTime(DateUtils.getNowDate());
-        return bookmarkLogMapper.insertBookmarkLog(bookmarkLog);
-    }
-
-    /**
-     * 修改书签操作日志
-     * 
-     * @param bookmarkLog 书签操作日志
-     * @return 结果
-     */
-    @Override
-    public int updateBookmarkLog(BookmarkLog bookmarkLog)
-    {
-        bookmarkLog.setUpdateTime(DateUtils.getNowDate());
-        return bookmarkLogMapper.updateBookmarkLog(bookmarkLog);
-    }
-
-    /**
-     * 批量删除书签操作日志
-     * 
-     * @param bookmarkLogIds 需要删除的书签操作日志主键
-     * @return 结果
-     */
-    @Override
-    public int deleteBookmarkLogByBookmarkLogIds(Long[] bookmarkLogIds)
-    {
-        return bookmarkLogMapper.deleteBookmarkLogByBookmarkLogIds(bookmarkLogIds);
-    }
-
-    /**
-     * 删除书签操作日志信息
-     * 
-     * @param bookmarkLogId 书签操作日志主键
-     * @return 结果
-     */
-    @Override
-    public int deleteBookmarkLogByBookmarkLogId(Long bookmarkLogId)
-    {
-        return bookmarkLogMapper.deleteBookmarkLogByBookmarkLogId(bookmarkLogId);
-    }
+public class BookmarkLogServiceImpl extends ServiceImpl<BookmarkLogMapper, BookmarkLog> implements  BookmarkLogService {
 }
