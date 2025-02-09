98af771c9f4ce3fd9939ce151b147f5ee816c702
isinvon <2021469084@qq.com>
2025-02-09 13:31:37 +0800

    refactor(admin): 移除 WebsiteInfoMapper 和 WebsiteInfoServiceImpl 中的冗余代码
    - 删除了 WebsiteInfoMapper 中的多余接口方法- 删除了 WebsiteInfoServiceImpl 中的多余实现方法
    - 简化了代码结构，提高了代码的可维护性和可读性

--- api/src/main/java/com/ruoyi/project/admin/mapper/WebsiteInfoMapper.java
+++ api/src/main/java/com/ruoyi/project/admin/mapper/WebsiteInfoMapper.java
@@ -1,64 +1,18 @@
 package com.ruoyi.project.admin.mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import java.util.List;
+
 import com.ruoyi.project.admin.domain.po.WebsiteInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 网站信息Mapper接口
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Mapper
-public interface WebsiteInfoMapper extends BaseMapper<WebsiteInfo>
-{
-    /**
-     * 查询网站信息
-     * 
-     * @param websiteInfoId 网站信息主键
-     * @return 网站信息
-     */
-    public WebsiteInfo selectWebsiteInfoByWebsiteInfoId(Long websiteInfoId);
-
-    /**
-     * 查询网站信息列表
-     * 
-     * @param websiteInfo 网站信息
-     * @return 网站信息集合
-     */
-    public List<WebsiteInfo> selectWebsiteInfoList(WebsiteInfo websiteInfo);
-
-    /**
-     * 新增网站信息
-     * 
-     * @param websiteInfo 网站信息
-     * @return 结果
-     */
-    public int insertWebsiteInfo(WebsiteInfo websiteInfo);
-
-    /**
-     * 修改网站信息
-     * 
-     * @param websiteInfo 网站信息
-     * @return 结果
-     */
-    public int updateWebsiteInfo(WebsiteInfo websiteInfo);
-
-    /**
-     * 删除网站信息
-     * 
-     * @param websiteInfoId 网站信息主键
-     * @return 结果
-     */
-    public int deleteWebsiteInfoByWebsiteInfoId(Long websiteInfoId);
-
-    /**
-     * 批量删除网站信息
-     * 
-     * @param websiteInfoIds 需要删除的数据主键集合
-     * @return 结果
-     */
-    public int deleteWebsiteInfoByWebsiteInfoIds(Long[] websiteInfoIds);
+public interface WebsiteInfoMapper extends BaseMapper<WebsiteInfo> {
 }
--- api/src/main/java/com/ruoyi/project/admin/service/impl/WebsiteInfoServiceImpl.java
+++ api/src/main/java/com/ruoyi/project/admin/service/impl/WebsiteInfoServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ruoyi.project.admin.service.impl;
+
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.project.admin.service.WebsiteInfoService;
 
 import java.util.List;
+
 import com.ruoyi.common.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,87 +15,10 @@ import org.springframework.stereotype.Service;
 
 /**
  * 网站信息Service业务层处理
- * 
+ *
  * @author sinvon
  * @date 2025-01-30
  */
 @Service
-public class WebsiteInfoServiceImpl extends ServiceImpl<WebsiteInfoMapper,WebsiteInfo> implements WebsiteInfoCustomService, WebsiteInfoService
-{
-    @Autowired
-    private WebsiteInfoMapper websiteInfoMapper;
-
-    /**
-     * 查询网站信息
-     * 
-     * @param websiteInfoId 网站信息主键
-     * @return 网站信息
-     */
-    @Override
-    public WebsiteInfo selectWebsiteInfoByWebsiteInfoId(Long websiteInfoId)
-    {
-        return websiteInfoMapper.selectWebsiteInfoByWebsiteInfoId(websiteInfoId);
-    }
-
-    /**
-     * 查询网站信息列表
-     * 
-     * @param websiteInfo 网站信息
-     * @return 网站信息
-     */
-    @Override
-    public List<WebsiteInfo> selectWebsiteInfoList(WebsiteInfo websiteInfo)
-    {
-        return websiteInfoMapper.selectWebsiteInfoList(websiteInfo);
-    }
-
-    /**
-     * 新增网站信息
-     * 
-     * @param websiteInfo 网站信息
-     * @return 结果
-     */
-    @Override
-    public int insertWebsiteInfo(WebsiteInfo websiteInfo)
-    {
-        websiteInfo.setCreateTime(DateUtils.getNowDate());
-        return websiteInfoMapper.insertWebsiteInfo(websiteInfo);
-    }
-
-    /**
-     * 修改网站信息
-     * 
-     * @param websiteInfo 网站信息
-     * @return 结果
-     */
-    @Override
-    public int updateWebsiteInfo(WebsiteInfo websiteInfo)
-    {
-        websiteInfo.setUpdateTime(DateUtils.getNowDate());
-        return websiteInfoMapper.updateWebsiteInfo(websiteInfo);
-    }
-
-    /**
-     * 批量删除网站信息
-     * 
-     * @param websiteInfoIds 需要删除的网站信息主键
-     * @return 结果
-     */
-    @Override
-    public int deleteWebsiteInfoByWebsiteInfoIds(Long[] websiteInfoIds)
-    {
-        return websiteInfoMapper.deleteWebsiteInfoByWebsiteInfoIds(websiteInfoIds);
-    }
-
-    /**
-     * 删除网站信息信息
-     * 
-     * @param websiteInfoId 网站信息主键
-     * @return 结果
-     */
-    @Override
-    public int deleteWebsiteInfoByWebsiteInfoId(Long websiteInfoId)
-    {
-        return websiteInfoMapper.deleteWebsiteInfoByWebsiteInfoId(websiteInfoId);
-    }
+public class WebsiteInfoServiceImpl extends ServiceImpl<WebsiteInfoMapper, WebsiteInfo> implements WebsiteInfoService {
 }
